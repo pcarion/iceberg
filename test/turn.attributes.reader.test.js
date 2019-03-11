a19c6e73a38aa55a3ea3b56cae461f71ef102070
Pierre Carion <pcarion@gmail.com>
2019-03-11 10:38:22 -0700

    add extra unit tests

--- test/turn.attributes.reader.test.js
+++ test/turn.attributes.reader.test.js
@@ -2,12 +2,22 @@ import CONSTANTS from '../src/turn/constants';
 import readAttribute from '../src/turn/attributes/reader';
 
 describe('turn.attributes.reader.test', () => {
-  test('parse valid MAPPED-ADDRESS', () => {
+  test('parse valid MAPPED-ADDRESS (1)', () => {
     const type = 0x0001;
     expect(readAttribute(type, Buffer.from('0001000A01020304', 'hex'))).toEqual({
       family: CONSTANTS.transport.address.family.IPv4,
       port: 10,
       address: parseInt('01020304', 16),
     });
+  });
+  test('parse invalid MAPPED-ADDRESS (1)', () => {
+    const type = 0x0001;
+    expect(() => readAttribute(type, Buffer.from('1001000A01020304', 'hex')))
+      .toThrowError(/The first 8 bits of the MAPPED-ADDRESS must be set to 0/);
+  });
+  test('parse invalid MAPPED-ADDRESS (2)', () => {
+    const type = 0x0001;
+    expect(() => readAttribute(type, Buffer.from('0003000A01020304', 'hex')))
+      .toThrowError(/invalid address family/);
   });
 });
